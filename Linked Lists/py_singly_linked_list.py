2da50ca30ff5f71a12e88c511a83b3fd60190bb7
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2021-02-25 13:19:22 -0500

    Create py_singly_linked_list.py
    Python version of linked list

---
+++ Linked Lists/py_singly_linked_list.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 08:57:46 2021
+
+@author: Ariel
+"""
+
+class Node:
+    def __init__(self, datavalue = None):
+        self.datavalue = datavalue
+        self.nextvalue = None
+    
+    def printvalue(self):
+        return self.datavalue
+
+class singly_linked_list:
+    def __init__(self):
+        self.headvalue = None
+        
+    def traverse(self):
+        
+        currentvalue = self.headvalue
+        while currentvalue is not None:
+            print(currentvalue.datavalue)
+            currentvalue = currentvalue.nextvalue
+    
+list1 = singly_linked_list()
+
+list1.headvalue = Node('mosquito')
+e2 = Node('dragonfly')
+e3 = Node('frog')
+e4 = Node('snake')
+e5 = Node('owl')
+e6 = Node('bacteria')
+e7 = Node('circle of life') #eventually
+
+list1.headvalue.nextvalue = e2
+
+e2.nextvalue = e3
+e3.nextvalue = e4
+e4.nextvalue = e5
+e5.nextvalue = e6
+e6.nextvalue = e7
