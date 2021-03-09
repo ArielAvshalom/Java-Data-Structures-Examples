4df634ef0fcb61e178a7e8eb7ae7697d563e77b2
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2021-03-09 10:44:31 -0500

    Recursion
    Added Recursion, Factorial and Fibonacci

---
+++ Recursion/Factorial.java
@@ -0,0 +1,17 @@
+//package Recursion;
+public class Factorial {
+    public static int fact(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        
+        System.out.println("our current value is " + n);
+
+        return n * fact(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(6));
+        
+    }
+}
---
+++ Recursion/fibonacci.java
@@ -0,0 +1,23 @@
+//package Recursion;
+
+public class fibonacci {
+    //0,1,1,2
+    public static int fib(int n) {
+        if (n == 2) {
+            return 1;
+        }
+        //it's much clearer to have several if and no nested elses if you can avoid.
+        if (n == 1){
+            return 0;
+        }
+        System.out.println("We are now in position " + n + " we will be doing fib on " + (n-1) +", " +  (n-2));
+        return fib(n - 1) + fib(n -2);
+        // 0,1,1,2 : 1 +1 = 2, 1+2 = 3, 2+3 = 5
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(10));
+        
+    }
+        
+}
