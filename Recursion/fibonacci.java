f534beb2307aca6cc960233f2622743604c0459e
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2021-03-11 11:00:37 -0500

    Update fibonacci.java
    Commented out the println

--- Recursion/fibonacci.java
+++ Recursion/fibonacci.java
@@ -10,13 +10,13 @@ public class fibonacci {
         if (n == 1){
             return 0;
         }
-        System.out.println("We are now in position " + n + " we will be doing fib on " + (n-1) +", " +  (n-2));
+        //System.out.println("We are now in position " + n + " we will be doing fib on " + (n-1) +", " +  (n-2));
         return fib(n - 1) + fib(n -2);
         // 0,1,1,2 : 1 +1 = 2, 1+2 = 3, 2+3 = 5
     }
 
     public static void main(String[] args) {
-        System.out.println(fib(10));
+        System.out.println(fib(1000));
         
     }
         
